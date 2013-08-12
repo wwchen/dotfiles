673ef04596219148de68ef6b8d774f453ab89f63
William Chen <williamchen16@gmail.com>
2013-08-12 12:57:00 -0500

    right side shows only the date

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
 
+# Iter 1 - back up originals, symlink files to github
+
 shopt -s dotglob # We want to glob dotfiles!
 
+apps=(bash vim git rails tmux irssi)
+
+##
+# Functions
+##
+
 # Symlink everything from here to whatever the current homedir is
-function preserve_stuff
+function preserve
 {
-	if [ -h ~/$1 ]; then
-		echo "~/$1 already exists, removing symlink"
-		rm ~/$1
-	elif [ -a ~/$1 ]; then
-		echo "~/$1 already exists, moving to: ~/$1.orig"
-		mv ~/$1 ~/$1.orig
-	fi
-	ln -s $PWD/$1 ~/$1
+  if [[ -f $1 ]]; then
+    mv -i $1 $1.orig
+  fi
 }
+
+##
+# Go through all da apps!
+##
+for app in "${apps[@]}"; do
+  if [[ $app == "bash" ]]; then
+    preserve .bashrc
+    cp -a .bashrc ~
+    break
+  fi
+  if [[ $app == "vim" ]]; then
+    preserve .vimrc
+    cp -a .vimrc ~
+mkdir ~/.vim
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+# put in the vundle part
+vim +BundleInstall +qall
+# put in the shortcuts
+    break
+  fi
+done
 
 # Glob all dotfiles with a few exceptions
 # Now I don't have to update this script much in the future!
