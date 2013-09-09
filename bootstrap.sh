24e4b72d6e8f68cebe51503ed04e2aa14692ad7a
William <williamchen16@gmail.com>
2013-09-09 13:25:52 -0500

    Ask before we act

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,68 +18,77 @@ function install {
   cp -a "$cwd/$src" "$HOME/$dst"
 }
 
-function setup {
-  name=$1
-  echo -en "\e[33mSet up $1? [Yn]\e[0m "
+function ask {
+  question=$1
+  echo -en "\e[33m$question?\e[0m [Yn]"
   read ans
   [[ -z $ans ]] && ans="y"
   if [[ $ans == "y" || $ans == "Y" ]]; then
-    # yes
-    echo yes
+    return 0  # return 0 for true
   elif [[ $ans == "n" || $ans == "N" ]]; then
-    # no
-    echo no
+    return 1  # return 1 for false
   else
-    # loop
     echo "Invalid response"
-    setup $1
+    ask $question
   fi
 }
 
 function notice {
   string=$*
   echo -e "\e[33m$string"
-  sleep 2
 }
 
-# check if it is debian we are running herer
+# TODO check if it is debian we are running here
 
-notice "Installing essential packages"
-sudo apt-get update
-sudo apt-get install git vim curl tmux uprecords-cgi vnstat
+if ask "Install essential packages"; then
+  sudo apt-get update
+  sudo apt-get install git vim curl tmux uprecords-cgi vnstat
+else notice "Skipped"; fi
 
-notice "Setup bash"
-sym-install .bash_profile
-sym-install .bashrc
-touch $HOME/.bash_custom
-notice "Put your custom crap in .bash_custom"
-# if we're setting up tmux and ssh, automatically attach the tmux session to bash profile
+if ask "Setup bash"; then
+  sym-install .bash_profile
+  sym-install .bashrc
+  touch $HOME/.bash_custom
+  notice "Put your custom crap in .bash_custom"
+  # if we're setting up tmux and ssh, automatically attach the tmux session to bash profile
+else notice "Skipped"; fi
 
-notice "Setup git"
-sym-install .gitignore_global
-git config --global user.name William Chen
-git config --global user.email williamchen16@gmail.com
-git config --global help.autocorrect 1
-git config --global core.excludesfile ~/.gitignore_global
-#http://adit.io/posts/2013-08-16-five-useful-git-tips.html
+if ask "Setup git"; then
+  sym-install .gitignore_global
+  git config --global user.name William Chen
+  git config --global user.email williamchen16@gmail.com
+  git config --global help.autocorrect 1
+  git config --global core.excludesfile ~/.gitignore_global
+  git config --global alias.latest "for-each-ref --count=10 --sort=-committerdate --format='%(committerdate:short) %(refname:short)'"
+  #http://adit.io/posts/2013-08-16-five-useful-git-tips.html
+else notice "Skipped"; fi
 
-notice "Setup tmux"
-sym-nstall .tmux.conf
+if ask "Setup tmux"; then
+  sym-install .tmux.conf
+else notice "Skipped"; fi
 
-notice "Setup ruby with rails"
-curl -L https://get.rvm.io | bash -s stable --rails
+if ask "Setup ruby with rails"; then
+  curl -L https://get.rvm.io | bash -s stable --rails
+else notice "Skipped"; fi
 
-echo "Setup vim with vundle"
-update-alternatives --config editor
-sym-install .vimrc
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
+if ask "Setup vim with vundle"; then
+  update-alternatives --config editor
+  sym-install .vimrc
+  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  vim +BundleInstall +qall
+fi
 
-echo "Setup ssh"
-ssh-keygen
-cat .ssh/id_rsa.pub >> "$cwd/ssh-authkeys"
-install ssh-authkeys .ssh/authorized_keys
+if ask "Setup ssh"; then
+  ssh-keygen
+  cat .ssh/id_rsa.pub >> "$cwd/ssh-authkeys"
+  install ssh-authkeys .ssh/authorized_keys
+else notice "Skipped"; fi
 
-echo "Setup iptables"
+if ask "Setup iptables"; then
+  install firewall.sh /root/firewall.sh
+  sudo /root/firewall.sh
+else notice "Skipped"; fi
 
-echo "Colorize root bash"
+if ask "Colorize root bash"; then
+  echo "blah"
+else notice "Skipped"; fi
